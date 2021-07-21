7dc99563ace9a35dd5ef8aafc99364c62928600c
wumengyong <1603317282@163.com>
2021-07-21 15:01:11 +0800

    沙箱安全机制示例（自定义String不能自定义加载器加载）

---
+++ src/main/java/java/lang/String.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+public class String {
+    String(){
+        System.out.println("自定义String类加载");
+    }
+
+    public void sout(){
+        System.out.println("自定义String类方法sout运行");
+    }
+}
